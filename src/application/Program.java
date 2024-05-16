a2ca8597702c0126ba8ec00d5f185482ad7b570f
Pedro Henrique <pedrodguimaraes20@gmail.com>
2024-05-16 16:27:02 -0300

    Insert implementatiton

--- src/application/Program.java
+++ src/application/Program.java
@@ -35,10 +35,15 @@ public class Program {
             System.out.println(obj);
         }
         
-         System.out.println("\n=== TEST 3: seller findByDepartment===");
+        System.out.println("\n=== TEST 3: seller findByDepartment===");
         list = sellerDao.findAll();
         for (Seller obj: list) {
             System.out.println(obj);
         }
+        
+        System.out.println("\n=== TEST 3: seller findByDepartment===");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id = " + newSeller.getId());
     }
 }
